c7f85bd07f2e50bff1a0940229e2b41ae37dfd52
JRC-Tojo <jrc.tojo@gmail.com>
2024-12-27 15:33:11 +0900

    ⚡️ Updated `createDummy.py` and Added `requirements.txt`

--- createDummy.py
+++ createDummy.py
@@ -1,14 +1,27 @@
-
 from pathlib import Path
-import shutil
 
-FOLDER_NAME = ['CAD', 'PDF']
+import pandas as pd
 
-for folder in FOLDER_NAME:
-  # �t�H���_���폜���đ|��
-  shutil.rmtree(Path(__file__).parent/folder)
-  # �V�����Đ���
-  (Path(__file__).parent/folder).mkdir()
-  # �t�@�C���̖��O���ЂƂÂ擾&�쐬
-  for path in (Path(__file__).parent/'secret'/folder).iterdir():
-    (Path(__file__).parent/folder/path.name).touch()
+
+def setNewTestFiles(targetFolderName: str, names: pd.Series):
+  """
+  指定したフォルダにテストファイルを作成する
+  """
+  targetPath = Path(__file__).parent/targetFolderName
+
+  # フォルダのリセット
+  for fPath in targetPath.iterdir():
+    fPath.unlink()
+
+  # 空ファイルを生成
+  names.map(lambda name: (targetPath/name).touch())
+
+
+# テストケースの読み込み
+loadTestCasePath = Path(__file__).parent / 'testCases' / 'by_山岸さん.xlsx'
+db = pd.read_excel(loadTestCasePath, sheet_name='図面ファイル対応一覧', index_col=0)
+
+
+# テストケースの作成
+setNewTestFiles('PDF', db['旧PDFファイル名（対応がないファイルも書いてOK・空欄禁止）'])
+setNewTestFiles('CAD', db['旧CADファイル名（対応がないファイルも書いてOK・空欄禁止）'])
